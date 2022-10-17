04a1fbc64d07deba0eae6810ea0e6cf134545f7c
Huy Nguyen <touch@nguyenrk.com>
2022-10-17 07:57:44 +0700

    Initial commit

---
+++ Hands/Hands/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Hands.ViewModels;
+
+namespace Hands.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
